e3d4ddbe75ed56b5fe9ad2243ab68e8c9d1fafad
Shubhangi509 <123101347+Shubhangi509@users.noreply.github.com>
2023-05-03 11:44:47 +0530

    Update opSize.cpp

--- opSize.cpp
+++ opSize.cpp
@@ -5,27 +5,26 @@ int opSize(string s)
     string one[] = {"HLT","MOV","STAX","XCHG","ADD","SUB","INR","DCR","INX","DCX","DAD","CMA","CMP"};
 	string two[] = {"MVI","ADI","SUI"};
 	string three[] = {"LXI","LDA","STA","SHLD","LHLD","JMP","JC","JNZ","JNC","JZ","SET"};
-	const char *ch = s.c_str();
-	char *var = (char*)ch;
-	const char *delimiter = " ,";
-	char *p = strtok (var,delimiter);
-	string query = p;
+	char *ch = s.c_str();
+	char *delimiter = " ,";
+	char *p = strtok (ch , delimiter);
+	string cmd = p;
 
     for(int i = 0 ; i < (sizeof(one) / sizeof(one[0])) ; i++)
     {
-        if(one[i] == query)
+        if(one[i] == cmd)
             return 1;
     }
 
     for(int i = 0 ; i < (sizeof(two) / sizeof(two[0])) ; i++)
     {
-        if(two[i] == query)
+        if(two[i] == cmd)
             return 2;
     }
 
     for(int i = 0 ; i < (sizeof(three) / sizeof(three[0])) ; i++)
     {
-        if(three[i] == query)
+        if(three[i] == cmd)
             return 3;
     }
     return 0;
