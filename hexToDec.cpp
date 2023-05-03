825c8e6d4e45545310efb9729cb9554bd828d853
Shubhangi509 <123101347+Shubhangi509@users.noreply.github.com>
2023-05-03 11:47:56 +0530

    Update hexToDec.cpp

--- hexToDec.cpp
+++ hexToDec.cpp
@@ -4,7 +4,7 @@
 void hexToDec(string pc,int a[])
 {
 	int p = 0;
-	for(int i =0 ;i < pc.length() ; i++)
+	for(int i = 0 ;i < pc.length() ; i++)
   {
 		if(pc[i] >= '0' && pc[i] <= '9')
 			a[i] = pc[i] - '0';
