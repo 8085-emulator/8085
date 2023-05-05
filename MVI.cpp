5a317f591e94476a09e14a9220f93dc2846ade4a
Shubhangi509 <123101347+Shubhangi509@users.noreply.github.com>
2023-05-05 21:16:20 +0530

    Update MVI.cpp

--- MVI.cpp
+++ MVI.cpp
@@ -2,26 +2,31 @@
 #include "ALLHEADER.h"
 #include "functions.h"
 
-MVI(string arg1 , string agr2 , string reg[] , bool flag[] , map<string , string> &mem)
+void MVI(string arg1 , string agr2 , string reg[] , bool flag[] , map<string , string> &mem)
 {
   if(arg1.length() == 1 && arg2.length() == 2)
   {
-    if(arg1 == 'M')
-    {
-      if(validData(arg2)))
-        mem[reg[5] + reg[6]] = arg2;
-      else
-      {
+     if(arg1 == "M")
+     {
+          if(validData(arg2))
+              mem[reg[5] + reg[6]] = arg2;
+          else
+          {
+              cout << "Error : Invalid content." << endl;
+              exit(0);
+          }
+     }
+     else if(validReg(arg1) && validData(arg2))
+       reg[regNo(arg1)] = arg2;
+     else
+     {
         cout << "Error : Invalid content." << endl;
         exit(0);
-      }
-    }
-    else if(validReg(arg1) && validData(arg2))
-      reg[regNo(arg1)] = arg2;
-    else
-    {
-      cout << "Error : Invalid content." << endl;
-      exit(0);
-    }
+     }
+  }
+  else
+  {
+    cout << "Error : Invalid content." << endl;
+    exit(0);
   }
 }
