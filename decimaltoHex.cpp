2e385bcb608372f5628844ef037caf85355ecee9
Dikshaaaaaaa <130124568+Dikshaaaaaaa@users.noreply.github.com>
2023-05-10 21:31:38 +0530

    Update decimaltoHex.cpp

--- decimaltoHex.cpp
+++ decimaltoHex.cpp
@@ -1,1 +1,15 @@
+#include "ALLMASTER.h"
 
+string decimalToHex(int deciml[])
+{
+	string hex="";
+	int size = *(&deciml+1)-deciml;
+	for(int i=0;i<size;i++)
+	{
+		if(deciml[i]>=0 && deciml[i]<=9)
+			hex = hex + char('0' + deciml[i]);
+		else if(deciml[i]>=10 && deciml[i]<=15)
+			hex+=(char)('A'+(deciml[i]-10));
+	}
+	return hex;
+}
