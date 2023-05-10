d0c7cff0a2c64b6685e926567cc05a4319ee845c
Shubhangi509 <123101347+Shubhangi509@users.noreply.github.com>
2023-05-10 22:04:12 +0530

    Create hexSub.cpp

---
+++ hexSub.cpp
@@ -0,0 +1,49 @@
+#include "ALLHEADER.h"
+#inlcude "functions.h"
+
+string hexSub(string arg1,string arg2,bool flag[],bool carry)
+{
+	string resultant = "";
+	int variable , parity;
+  int value1[2] , value2[2] , tempAnswer[2];
+	hexToDec(arg1 , value1);
+	hexToDec(arg2 , value2);
+	if(value1[1] < value2[1])
+  {
+      tempAnswer[1] = (16+value1[1])-value2[1];
+		  --value1[0];
+	}
+	else
+	  	tempAnswer[1] = value1[1] - value2[1];
+	if(value1[0] < value2[0])
+  {
+		  if(carry == true)
+  		  	flag[0] = true;
+		  tempAnswer[0] = (16+value1[0]-value2[0]);
+	}
+	else
+	  	tempAnswer[0] = value1[0] - value2[0];
+	variable = tempAnswer[0]*16 + tempAnswer[1];
+	bitset<8> dataInBinary(variable);
+	parity = dataInBinary.count();
+	if(parity % 2 == 0 && parity!=0)
+  		flag[2] = true;
+	else
+	  	flag[2] = false;
+
+  flag[7] = dataInBinary[7];
+  
+	if(parity == 0)
+	  	flag[6] = true;
+	else
+  		flag[6] = false;
+
+  for(int i = 1 ; i >= 0 ; --i)
+  {
+		  if(tempAnswer[i] >= 0 && tempAnswer[i] <= 9)
+  			  resultant = char('0' + tempAnswer[i]) + resultant;
+		  else if(tempAnswer[i]>=10 && tempAnswer[i]<=15)
+			    resultant = (char)('A'+(tempAnswer[i] - 10)) + resultant;
+	}
+	return resultant;
+}
