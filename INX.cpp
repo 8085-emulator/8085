7a74d04b76e5a6881ff5e6ce3295acc6a27ea244
Shubhangi509 <123101347+Shubhangi509@users.noreply.github.com>
2023-05-11 22:46:06 +0530

    Update INX.cpp

--- INX.cpp
+++ INX.cpp
@@ -4,45 +4,45 @@
 void INX(string arg , string reg[] , bool flag[])
 {
 	if(arg.length() == 1)
-  {
-			if(validReg(arg))
-      {
-					int registerID = regNo(arg);
-			    if(registerID == 1 || registerID == 3 || registerID == 5)
-          {
+	{
+		if(validReg(arg))
+      		{
+ 		    	int registerID = regNo(arg);
+		    	if(registerID == 1 || registerID == 3 || registerID == 5)
+ 	         	{
     				string data16b = "";
 		    		string temp = "";	
-				    string s1 = "";
+			        string s1 = "";
     				string s2 = "";
 		    		if(validData(reg[registerID]) == true && validData(reg[registerID + 1]) == true)
-            {
-								data16b = data16b + reg[registerID] + reg[registerID + 1];
-      					temp = increaseAddress(data16b);
-       				  s1 = s1 + temp[0] + temp[1];		
-    					  s2 = s2 + temp[2] + temp[3];
-    					  reg[registerID] = s1; 
-					      reg[registerID + 1] = s2;
-				    }
+			        {
+					data16b = data16b + reg[registerID] + reg[registerID + 1];
+      					temp = incrAddr(data16b);
+       					s1 = s1 + temp[0] + temp[1];		
+    					s2 = s2 + temp[2] + temp[3];
+    					reg[registerID] = s1; 
+					reg[registerID + 1] = s2;
+				}
     				else
-            {
-								cout << "Error : Invalid data content in registers.\nThe program will quit\n";
+            			{
+					cout << "Error : Invalid data content in registers.\nThe program will quit\n";
     					exit(0);
     				}
-			   }
-    		 else
-         {
-				    cout << "Error : Invalid register pair.\nThe program will quit\n";
+			}
+    	 	   	else
+         		{
+				cout << "Error : Invalid register pair.\nThe program will quit\n";
     				exit(0);
-    		 }
+ 	   		}
 		  }
 		  else
-      {
-			    cout << "Error : Invalid register content.\nThe program will quit\n";
+      		  {
+			cout << "Error : Invalid register content.\nThe program will quit\n";
     			exit(0);
-  		}
+  		  }
 	}
 	else
-  {
+	{
 	  	cout << "Error : Invalid input arguments.\nThe program will quit\n";
   		exit(0);
 	}
